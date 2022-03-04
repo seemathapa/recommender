1ecf1019649139a2bbef5e5aaf1704bc0eb7ca63
seemathapa <tseema@insightworkshop.io>
2022-03-04 15:37:32 +0545

    change the name of navbar

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -100,7 +100,7 @@ export default function Header() {
               Find a Book
             </a>
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Latest trends
+              Trends
             </a>
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Blog
